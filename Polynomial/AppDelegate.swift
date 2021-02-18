ac8b10753eee347af653a0f9b58db4cdb21ab98e
Mike Cargal <mike@cargal.net>
2021-02-18 08:42:50 -0500

    Initial Commit

---
+++ Polynomial/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Polynomial
+//
+//  Created by Mike Cargal on 2/18/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Polynomial/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Polynomial
+//
+//  Created by Mike Cargal on 2/18/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
