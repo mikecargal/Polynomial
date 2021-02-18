01e07b8f18208b77d1c43fccbd40d5f4b0d9e8a0
Mike Cargal <mike@cargal.net>
2021-02-18 18:13:57 -0500

    parse from input field

--- Polynomial/ViewController.swift
+++ Polynomial/ViewController.swift
@@ -9,17 +9,31 @@ import Antlr4
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet weak var textInput: NSTextField!
+    @IBOutlet weak var ParseButton: NSButton!
+    @IBOutlet weak var messageLabel: NSTextField!
+    @IBAction func parseClicked(_ sender: NSButton) {
+        print("parse \"\(textInput.stringValue)\"")
+        parseInput()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let inputStream = ANTLRInputStream("(x^5 + -1) * (2*x^2+3)+1/2*x")
+        
+        let polyText = "(x^5 + -1) * (2*x^2+3)+1/2*x"
+        textInput.stringValue = polyText
+    }
+    
+    func parseInput() {
+        let source = textInput.stringValue
+        let inputStream = ANTLRInputStream(source)
         let lexer = PolyLexer(inputStream)
         let tokenStream = CommonTokenStream(lexer)
-
         do {
             let parser = try PolyParser(tokenStream)
             let poly = try parser.pExpr()
             print("parsed \(poly.getText())")
+            messageLabel.stringValue = "Succesful Parse"
             print("\(poly.debugDescription)")
             let walker = ParseTreeWalker()
             let listener = MyPolyListener()
@@ -27,6 +41,7 @@ class ViewController: NSViewController {
 
         } catch {
             print("error creating Parser (\(error))")
+            messageLabel.stringValue = error.localizedDescription
         }
     }
 
